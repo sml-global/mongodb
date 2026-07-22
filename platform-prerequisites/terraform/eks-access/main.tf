c0de8d1d62602cc4e3710cccd3c2da838ef9b673
sml-admin <github_admin@sml.com>
2026-07-22 09:27:07 +0800

    test: harden UAT EKS access bindings

--- platform-prerequisites/terraform/eks-access/main.tf
+++ platform-prerequisites/terraform/eks-access/main.tf
@@ -21,7 +21,7 @@ resource "aws_eks_access_policy_association" "cluster_admin" {
   ])
 
   cluster_name  = var.eks_cluster_name
-  principal_arn = aws_eks_access_entry.workforce[each.value].principal_arn
+  principal_arn = aws_eks_access_entry.workforce[each.key].principal_arn
   policy_arn    = "arn:aws:eks::aws:cluster-access-policy/AmazonEKSClusterAdminPolicy"
 
   access_scope {
