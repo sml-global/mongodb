e5fefd7f251c2981a6bbe5e14ef83e6404b4677b
sml_admin <sml_admin@sml.local>
2026-07-29 18:22:26 +0800

    feat: add S3 Gateway VPC Endpoint to avoid NAT Gateway costs for backup traffic

--- platform-prerequisites/terraform/modules/network/main.tf
+++ platform-prerequisites/terraform/modules/network/main.tf
@@ -153,3 +153,20 @@ resource "aws_route_table_association" "database" {
   subnet_id      = each.value.id
   route_table_id = aws_route_table.database[each.key].id
 }
+
+data "aws_region" "current" {}
+
+resource "aws_vpc_endpoint" "s3" {
+  vpc_id            = aws_vpc.this.id
+  service_name      = "com.amazonaws.${data.aws_region.current.name}.s3"
+  vpc_endpoint_type = "Gateway"
+
+  route_table_ids = concat(
+    [for rt in aws_route_table.private : rt.id],
+    [for rt in aws_route_table.database : rt.id],
+  )
+
+  tags = {
+    Name = "${var.name_prefix}-s3-endpoint"
+  }
+}
